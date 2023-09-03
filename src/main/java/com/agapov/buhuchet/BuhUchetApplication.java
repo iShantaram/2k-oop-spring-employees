a0d9f8b52d7bedaa9b616c97a3506c97c352aae0
iShantaram <1agapovav987@gmail.com>
2023-09-03 15:31:47 +0300

    Init commit

---
+++ src/main/java/com/agapov/buhuchet/BuhUchetApplication.java
@@ -0,0 +1,13 @@
+package com.agapov.buhuchet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuhUchetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuhUchetApplication.class, args);
+	}
+
+}
