853b4ddc80604052fc2df7a44dadcd5cccbd3d08
iShantaram <1agapovav987@gmail.com>
2023-09-03 16:26:23 +0300

    Реализованы функции добалвения, удаления и поиска сотрудников в списке, по имени и фамилии. Статусы ошибок обработаны через ExceptionHandler.

---
+++ src/main/java/com/agapov/buhuchet/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.agapov.buhuchet.service;
+
+import com.agapov.buhuchet.domain.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstname, String lastname);
+    Employee removeEmployee(String firstname, String lastname);
+    Employee findEmployee(String firstname, String lastname);
+    List<Employee> dataList();
+}
